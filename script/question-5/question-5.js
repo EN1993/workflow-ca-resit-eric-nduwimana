0d6647395e4cc09249a4c91ec9d32c11bf5eb4e2
en1993 <74547465+EN1993@users.noreply.github.com>
2022-06-16 15:11:34 +0200

    add

---
+++ script/question-5/question-5.js
@@ -0,0 +1,58 @@
+// API url: https://graphqlzero.almansi.me/api
+
+import {orderBy} from 'lodash'
+
+import 'regenerator-runtime/runtime';
+
+const userList = document.querySelector('.results-list');
+
+import ApolloClient, { gql } from 'apollo-boost';
+
+async function myGQL() {
+  
+
+  
+const client = new ApolloClient({
+  uri: 'https://graphqlzero.almansi.me/api'
+});
+const json = await client.query({ query: gql`
+{
+  posts {
+    data {
+      id
+      title
+    }
+  }
+}
+  
+
+`}).then(console.log);
+
+const orderedPosts = orderBy(await json.data.posts.data, ["id"], ["desc"]);
+
+  console.log('NEW POST:', orderedPosts);
+
+ 
+
+
+  for (let i = 0; i < orderedPosts.length; i++) {
+
+
+
+    if (i === 5) {
+      break;
+    }
+    
+    // console.log('NEW POST:', orderedPosts);
+
+
+  }
+
+
+}
+
+myGQL()
+  
+
+ 
+
